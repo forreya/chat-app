bd0a09266fd7e1392bf58d0628a4edcd1e5d37e1
Ryan Lai <ryanlai12@gmail.com>
2023-05-18 18:51:06 +0800

    Setup register backend endpoint

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const UserModel = require('./models/User')
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const MONGO_URL = process.env.MONGO_URL;
+JWT_SECRET = process.env.JWT_SECRET;
 
 mongoose.connect(MONGO_URL);
 
@@ -15,8 +18,13 @@ app.get('/test', (req,res) => {
   res.json('test ok');
 })
 
-app.post('/register', (req,res) => {
-
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  const createdUser = await UserModel.create({username, password});
+  jwt.sign({userId: createdUser, _id}, JWT_SECRET, (error, token) => {
+    if (error) throw error;
+    res.cookie('token', token).status(201).json('ok')
+  })
 })
 
 app.listen(PORT);
