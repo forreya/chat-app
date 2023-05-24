3edd87541e4b1f936318677744494af841548eae
Ryan Lai <ryanlai12@gmail.com>
2023-05-24 14:11:09 +0800

    Created chat component

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,13 @@
 import RegisterAndLogin from "./pages/RegisterAndLogin"
 import { useContext } from "react"
 import { UserContext } from "./state/userContext"
+import Chat from "./components/Chat"
 
 export default function Routes() {
   const {username, id} = useContext(UserContext)
 
   if (username) {
-    return 'Hello there, ' + username;
+    return <Chat />
   }
 
   return (
