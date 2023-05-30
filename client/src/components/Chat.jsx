b33115dc42810d807e328a54d41f021ae515b626
Ryan Lai <ryanlai12@gmail.com>
2023-05-30 14:16:51 +0800

    Fixed padding and overall display of blue bar in messaging contacts

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -33,6 +33,10 @@ export default function Chat() {
 
   const onlinePeopleExclOurUser = {...onlinePeople}
   delete onlinePeopleExclOurUser[id]
+  console.log('All users')
+  console.log(onlinePeople)
+  console.log('Excluding logged in:')
+  console.log(onlinePeopleExclOurUser)
 
   return (
     <div className="flex h-screen">
@@ -43,9 +47,14 @@ export default function Chat() {
           <div 
             key={userId}
             onClick={() => setSelectedUserId(userId)} 
-            className={"border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer "+(userId === selectedUserId ? 'bg-blue-50' : '')}>
-            <Avatar username={onlinePeople[userId]} userId={userId} />
-            <span className="text-gray-800">{onlinePeople[userId]}</span>
+            className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer "+(userId === selectedUserId ? 'bg-blue-50' : '')}>
+            {userId === selectedUserId && (
+              <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+            )}
+            <div className="flex gap-2 py-2 pl-4 items-center"> 
+              <Avatar username={onlinePeople[userId]} userId={userId} />
+              <span className="text-gray-800">{onlinePeople[userId]}</span>
+            </div>
           </div>
         ))}
        </div>
